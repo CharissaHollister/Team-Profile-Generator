2881c271cafd92d2805f9e57e5b9d6703eda081d
CharissaHollister <charissa.hollister@diasorin.com>
2022-06-17 19:12:58 -0500

    set up backbone files

---
+++ index.js
@@ -0,0 +1,12 @@
+// Initiates and runs the program 
+
+const inquirer = require("inquirer");
+const generatePage = require("./lib/generatePage");
+const teamMember = require("./src/teamMember");
+
+// prompted to enter the team manager’s name, employee ID, email address, and office number
+
+
+
+// WHEN I decide to finish building my team
+// THEN I exit the application, and the HTML is generated
---
+++ src/generatePage.js
@@ -0,0 +1,1 @@
+// generate a webpage (html) that displays my team's basic info (email, github)
